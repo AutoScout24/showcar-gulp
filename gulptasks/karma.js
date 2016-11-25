66687565b236efdfd64d5adf96dfc544c3e8c2dc
Adam <aberesdeak@autoscout24.com>
2016-11-25 11:45:21 +0100

    improved karma

--- gulptasks/karma.js
+++ gulptasks/karma.js
@@ -3,26 +3,21 @@ const KarmaServer = require('karma').Server;
 const globalConfig = require('../global-config');
 
 module.exports = (gulp, options, done) => {
-    const files = options.files//.map(f => path.resolve(process.cwd(), f));
+    const files = options.files;
     files.push({ pattern: '**/*.js.map', included: false, watched: false });
+
+    const frameworks = ['mocha', 'chai', 'sinon'].concat(options.frameworks || []);
+    const plugins = ['karma-mocha-reporter', 'karma-mocha', 'karma-sinon', 'karma-chai', 'karma-electron'].concat(options.plugins || []);
 
     new KarmaServer({
         basePath: process.cwd(),
         files,
         browsers: ['Electron'],
         singleRun: !globalConfig.devmode,
-        // configFile: path.resolve(__dirname, '../karma.conf.js'),
-
-        // client: { useIframe: false },
         reporters: ['mocha'],
-        frameworks: ['mocha', 'chai', 'sinon'],//.concat(options.frameworks || []),
-        plugins: [
-            'karma-mocha-reporter',
-            'karma-mocha',
-            'karma-sinon',
-            'karma-chai',
-            'karma-electron'
-        ], //.concat(options.plugins || []),
+        frameworks,
+        plugins,
+        // client: { useIframe: false },
         client: {
             mocha: {
                 reporter: 'html'
