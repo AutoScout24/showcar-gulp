d5d8ba04ec71e882399c28e72eae9c5309219716
Andrei Solovev <andrei.solovev@scout24.com>
2017-04-11 16:18:26 +0200

    add ie edge browserstack remove sauce-labs and tune karma

--- gulptasks/karma.js
+++ gulptasks/karma.js
@@ -8,13 +8,13 @@ module.exports = (gulp, options, done) => {
     files.push({ pattern: '**/*.js.map', included: false, watched: false });
 
     const frameworks = ['mocha', 'chai', 'sinon', 'source-map-support', 'browserify'].concat(options.frameworks || []);
-    const plugins = ['karma-mocha-reporter', 'karma-mocha', 'karma-sinon', 'karma-chai', 'karma-electron', 'karma-sauce-launcher', 'karma-firefox-launcher', 'karma-safari-launcher', 'karma-chrome-launcher', 'karma-browserify', 'karma-source-map-support'].concat(options.plugins || []);
+    const plugins = ['karma-mocha-reporter', 'karma-mocha', 'karma-sinon', 'karma-chai', 'karma-electron', 'karma-sauce-launcher', 'karma-browserstack-launcher', 'karma-firefox-launcher', 'karma-safari-launcher', 'karma-chrome-launcher', 'karma-browserify', 'karma-source-map-support'].concat(options.plugins || []);
 
     const proxies = options.proxies || '';
     const urlRoot = options.proxies ? '/karma/' : '/'; // if  proxy, move karma to another url
     const preprocessors = options.preprocessors || '';
     let karmaConfig = {
-        // logLevel: 'LOG_DEBUG',
+        // logLevel: 'LOG_DEBUG', //keep for debugging
         browserConsoleLogOptions: {
             level: 'log',
             terminal: true
@@ -26,67 +26,73 @@ module.exports = (gulp, options, done) => {
         preprocessors,
         port: 9876, //fix port, don't change
         urlRoot,
+        proxies,
         browserify: {
             debug: true, // include inline source maps
             transform: ['require-globify'],
             plugin: [['sourcemapify', { 'root': '/' }]]
         },
-        proxies,
-        singleRun: true,
-        browserNoActivityTimeout: 24 * 60000,
-        // use an extended timeout for capturing Sauce Labs
-        // browsers in case the service is busy
-        captureTimeout: 12 * 60000,
-        browserDisconnectTolerance: 3,
     };
 
-    const saucelabsCustomLaunchers = {
-        chrome_latest: {
-            base: 'SauceLabs',
-            browserName: 'chrome',
-            version: 'latest'
+    const browserStackCustomLaunchers = {
+        bs_safari_mac: {
+            base: 'BrowserStack',
+            browser: 'Safari',
+            browser_version: '10.0',
+            os: 'OS X',
+            os_version: 'Sierra'
         },
-        firefox_latest: {
-            base: 'SauceLabs',
-            browserName: 'firefox',
-            version: 'latest'
+        bs_chrome_win: {
+            base: 'BrowserStack',
+            browser: 'Chrome',
+            browser_version: '57.0',
+            os: 'Windows',
+            os_version: '10'
         },
-        /* safari_10: {
-         base: 'SauceLabs',
-         browserName: 'safari',
-         platform: 'OS X 10.11',
-         version: '10.0'
-         },*/
-        /* ie_11: {
-         base: 'SauceLabs',
-         browserName: 'internet explorer',
-         platform: 'Windows 7',
-         version: '11'
-         },*/
-        /* edge: {
-         base: 'SauceLabs',
-         browserName: 'MicrosoftEdge',
-         platform: 'Windows 10',
-         version: 'latest'
-         }*/
+        bs_firefox_win: {
+            base: 'BrowserStack',
+            browser: 'Firefox',
+            browser_version: '52.0',
+            os: 'Windows',
+            os_version: '10'
+        },
+        bs_edge_win: {
+            base: 'BrowserStack',
+            browser: 'Edge',
+            browser_version: '14.0',
+            os: 'Windows',
+            os_version: '10'
+        },
+        bs_ie11_win: {
+            base: 'BrowserStack',
+            browser: 'IE',
+            browser_version: '11.0',
+            os: 'Windows',
+            os_version: '10'
+        }
     }
 
-    if (options.sauceLabs) {
-        karmaConfig.sauceLabs = options.sauceLabs;
-        karmaConfig.customLaunchers = options.customLaunchers ? options.customLaunchers : saucelabsCustomLaunchers;
-        karmaConfig.browsers = options.browsers ? options.browsers : Object.keys(karmaConfig.customLaunchers);
-        karmaConfig.reporters = options.reports || ['mocha', 'saucelabs'];
+    if (options.browserStack) {
+        karmaConfig.browserStack = options.browserStack || {
+                username: process.env.BROWSERSTACK_USERNAME,
+                accessKey: process.env.BROWSERSTACK_ACCESS_KEY
+            };
+        karmaConfig.customLaunchers = options.customLaunchers ? options.customLaunchers : browserStackCustomLaunchers;
+        karmaConfig.browsers = options.browsers ? options.browsers : ['bs_edge_win', 'bs_safari_mac', 'bs_firefox_win', 'bs_ie11_win', 'bs_chrome_win'];
+        karmaConfig.reporters = options.reports || ['mocha'];
         karmaConfig.concurrency = options.concurrency || karmaConfig.browsers.length;
-        // karmaConfig.concurrency = 1;
+        karmaConfig.concurrency = 1; //keep temporary
     } else {
         karmaConfig.browsers = options.browsers || ['Electron'];
         karmaConfig.reporters = options.reports || ['mocha'];
-        karmaConfig.singleRun = ! globalConfig.devmode;
+        karmaConfig.singleRun = ! globalConfig.devmode || options.singleRun;
+        karmaConfig.singleRun = true; //keep temporary
     }
     karmaConfig.client = {
         captureConsole: true,
         mocha: {
             reporter: 'html',
+            timeout: 10000
         }
     };
 
